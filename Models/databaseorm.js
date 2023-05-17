581034624159d6f31d18d0c1d94a2f7324ae6bde
Richard <rkklinks@gmail.com>
2023-05-17 21:00:35 +0000

    take-up

--- Models/databaseorm.js
+++ Models/databaseorm.js
@@ -1,38 +1,42 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
+
 const sequelize = new Sequelize('postgres://postgres:admin@localhost:5432/phonebook');
 
-try {
-    sequelize.authenticate();
+async function authenticateDatabase() {
+  try {
+    await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+}
 
 // Define a new model for the "users" table
 const User = sequelize.define('phone_user', {
-    // Model attributes are defined here
-    id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
-      },
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    lastName: {
-      type: DataTypes.STRING
-      // allowNull defaults to true
-    }
-  }, {
-   
-  });
-  
-  // `sequelize.define` also returns the model
-  User.sync();
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+});
+
+async function syncUserModel() {
+  await User.sync();
   console.log("The table for the Phonebook model was just (re)created!");
+}
 
+// Authenticate the database connection
+authenticateDatabase();
 
-  module.exports = { User } 
+// Synchronize the User model with the database
+syncUserModel();
 
-
+module.exports = { User };

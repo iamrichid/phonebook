e29e7f9d520156d8c87860853e0d270324019fb7
Richard <rkklinks@gmail.com>
2023-05-17 20:03:30 +0000

    code formatting

--- src/server.js
+++ src/server.js
@@ -1,85 +1,63 @@
-const express = require('express')
-const app = express()
-const {User} =require('./../Models/databaseorm')
-const bodyParser = require('body-parser')
-const { where } = require('sequelize')
-const port = 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const { User } = require('./../Models/databaseorm');
 
+const app = express();
+const port = 5000;
 
-// playing around
-// const richid = User.build({ firstName: "Richard", lastName: "Narh" });
-//   console.log("Richid's auto-generated ID:", richid.id);
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
-//   richid.save()
-app.use(bodyParser.urlencoded({extended:false}))
-app.set('view engine', 'ejs')
+// Routes
+app.get('/', (req, res) => {
+  User.findAll().then(users => {
+    const contact = JSON.parse(JSON.stringify(users));
+    console.log(users);
+    res.render('index', { contact });
+  });
+});
 
-app.get('/', (req , res) => {
-    // let phonebook  = Object.values(data.phonebook);
-    const users = User.findAll().then(users => {
-        const jsonData = JSON.stringify(users);
-        const phonebook = JSON.parse(jsonData)
-        console.log(users)
-        res.render('index', {phonebook : phonebook })
-        ;})   
-})
+app.get('/new_contact', (req, res) => {
+  res.render('new');
+});
 
-app.get('/new_phonebook', (req , res)=> {
-    res.render('new')
-})
+app.get('/edit_contact/:id', (req, res) => {
+  const id = req.params.id;
+  User.findByPk(id).then(users => {
+    const contact = JSON.parse(JSON.stringify(users));
+    res.render('edit', { contact });
+  });
+});
 
-app.get('/edit_phonebook/:id', (req , res)=> {
-    id = req.params.id
-   
-    const users = User.findByPk(id).then( users => {
-        const jsonData = JSON.stringify(users); 
-        const phonebook = JSON.parse(jsonData) 
-        res.render('edit', {phonebook : phonebook})
-    })
+app.post('/save-form', (req, res) => {
+  const { firstname, lastname } = req.body;
+  const new_user = User.build({ firstName: firstname, lastName: lastname });
+  new_user.save();
+  res.redirect('/');
+});
 
-})
-
-app.post('/save-form', function(req, res) {
-    
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-
-    const new_user = User.build({firstName : firstname , lastName : lastname})
-    
-    new_user.save()
-    
+app.post('/update-form', (req, res) => {
+  const { id, firstname, lastname } = req.body;
+  console.log(`${id} ${firstname} and ${lastname}`);
+  User.update(
+    { firstName: firstname, lastName: lastname },
+    { where: { id: id } }
+  ).then(() => {
     res.redirect('/');
-  
+  });
 });
 
-app.post('/update-form', function(req, res) {
-    const id = req.body.id;
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    console.log(`${id} ${firstname} and ${lastname}`) 
-
-   const users = User.update(
-        {firstName : firstname , lastName : lastname},
-        {where : {id: id}}).then(users => {
-
-        res.redirect('/');
-        })
+app.get('/delete_contact/:id', (req, res) => {
+  const id = req.params.id;
+  User.findByPk(id).then(users => {
+    const contact = JSON.parse(JSON.stringify(users));
+    console.log(contact);
+    users.destroy();
+    res.redirect('/');
+  });
 });
 
-app.get('/delete_phonebook/:id', function(req, res) {
-    const id = req.params.id;
-    console.log(id)
-
-    const users = User.findByPk(id).then( users => {
-        const jsonData = JSON.stringify(users); 
-        const phonebook = JSON.parse(jsonData) 
-        console.log(phonebook)
-        users.destroy()
-        res.redirect('/');
-    })
-  
+app.listen(port, () => {
+  console.log(`contacts app listening on port ${port}`);
 });
-
-app.listen(port , () => {
-    console.log(`Phonebook app listening on port ${port}`)
-})
